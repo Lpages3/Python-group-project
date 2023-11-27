dbfb69f948385e5932f1c729f4b48ccb8446afeb
Ako-minami <146741691+Ako-minami@users.noreply.github.com>
2023-11-27 11:44:08 +0100

    proteins py
    j'ai finit le script proteins.py

--- words_in_proteome.py
+++ words_in_proteome.py
@@ -19,6 +19,39 @@ def read_words():
         print("File not found.")
     
     return mots_filtres
-
-
 read_words()
+def read_sequences(file_path):
+    sequences = {}
+    with open(file_path, 'r') as file:
+        lines = file.readlines()
+        identifier = None
+        sequence = ''
+        for line in lines:
+            if line.startswith('>'):  # Identifying the protein identifier
+                if identifier:
+                    sequences[identifier] = sequence
+                identifier = line.strip().split('|')[1]  # Extracting protein identifier
+                sequence = ''
+            else:  # Accumulating protein sequence
+                sequence += line.strip()
+        if identifier and sequence:  # Adding the last protein sequence
+            sequences[identifier] = sequence
+    return sequences
+
+def main():
+    file_path = "/Users/lucyarnaud/Desktop/cours_M1/Github_project/Python-group-project/proteins.py"  # Replace with the actual file path
+    sequences = read_sequences(file_path)
+    
+    # Display the number of sequences read
+    print(f"Number of sequences read: {len(sequences)}")
+    
+    # Display the sequence associated with protein O95139
+    protein_id = 'O95139'
+    if protein_id in sequences:
+        print(f"Sequence for protein {protein_id}:")
+        print(sequences[protein_id])
+    else:
+        print(f"No sequence found for protein {protein_id}")
+
+if __name__ == "__main__":
+    main()
