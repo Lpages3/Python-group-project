04c624a7e647a5b471a4b7400f57551cd2ba36ac
maely-s <146752357+maely-s@users.noreply.github.com>
2023-12-05 20:11:52 +0100

    ajout de search for words + titres

--- words_in_proteome.py
+++ words_in_proteome.py
@@ -1,3 +1,5 @@
+#WORDS
+
 def read_words():
     
     a = 'english-common-words.txt'
@@ -14,7 +16,8 @@ def read_words():
         long_mots_filtres = len(mots_filtres)
         print("The number of selected wotds are :",long_mots_filtres)
         
-    
+#PROTEINS
+   
     except FileNotFoundError:
         print("File not found.")
 def read_sequences(file_path):
@@ -54,4 +57,50 @@ def main():
 if __name__ == "__main__":
     main()
 
+#SEARCHING FOR WORDS 
+
+def read_fasta(file_path):
+    sequences = {}
+    current_sequence = ""
+    current_id = None
+
+    with open(file_path, 'r') as file:
+        for line in file:
+            line = line.strip()
+            if line.startswith('>'):
+                # New sequence, register previous one if it exist
+                if current_id:
+                    sequences[current_id] = current_sequence
+                current_id = line[1:]  # the sequence identifier is retrieved
+                current_sequence = ""  # reset the current sequence
+            else:
+                # Adding the line to the current sequence
+                current_sequence += line
+
+        # Save the last sequence after playing the file
+        if current_id:
+            sequences[current_id] = current_sequence
+
+    return sequences
+
+def search_words_in_proteome(file_path):
+    protein_sequences = read_fasta(file_path)
+    sequences_with_words = {word: 0 for word in word_list}
+
+    for word in word_list:
+        for sequence_id, sequence in protein_sequences.items():
+            if word in sequence:
+                sequences_with_words[word] += 1
+                #print(f"{word} found in sequence {sequence_id}")
+
+    # Display of requested messages
+    for word, count in sequences_with_words.items():
+        print(f"{word} found in {count} sequences")
+
+    return sequences_with_words
+
+word_list = ['ACCESS', 'ACID', 'ACT']
+
+# To write in terminal
+search_words_in_proteome('human-proteome.fasta')
 
