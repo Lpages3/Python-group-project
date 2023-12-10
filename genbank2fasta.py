6ae930d8a016d9917176b046a24750d1a099bc83
Ako-minami <146741691+Ako-minami@users.noreply.github.com>
2023-12-10 19:25:15 +0100

    Update genbank2fasta.py
    C'est bon ça marche youpiii

--- genbank2fasta.py
+++ genbank2fasta.py
@@ -21,7 +21,7 @@ def extract_organism(file_content):
     return organism_name
 
 def main():
-    file_name = 'NC_001133.GBK'  # Replace with your GenBank file name
+    file_name = 'NC_001133.GBK'  #GenBank file
     file_content = read_file(file_name)
     
     if file_content:
@@ -45,18 +45,22 @@ def find_genes(file_content):
     genes = []
 
     for line in file_content:
-        if line.startswith('     gene            '):  # Check if the line indicates a gene
-            is_antisense = 'complement' in line  # Check if it's an antisense gene
-            line = line.replace('     gene            ', '')  # Remove unnecessary prefix
-            line = line.replace('<', '').replace('>', '')  # Remove < and > symbols
-            start, end = map(int, line.split('..'))  # Split and convert to integers
-
-            if is_antisense:
-                genes.append([start, end, 'antisense'])
-            else:
-                genes.append([start, end, 'sense'])
+        if line.startswith('     gene            '):
+            is_antisense = 'complement' in line
+            line = line.replace('     gene            ', '')
+            line = line.replace('<', '').replace('>', '').replace('complement(', '').replace(')', '')  # Remove 'complement('
+            start_end = line.split('..')
+            
+            if len(start_end) == 2:
+                start, end = map(int, start_end)
+                
+                if is_antisense:
+                    genes.append([start, end, 'antisense'])
+                else:
+                    genes.append([start, end, 'sense'])
 
     return genes
+
 
 def main():
     file_name = 'NC_001133.GBK'  # Replace with your GenBank file name
@@ -92,7 +96,7 @@ if __name__ == "__main__":
 #EXTRACTING THE NUCLEOTIDE SEQUENCE OF THE GENOME
 
 def extract_sequence(file_content):
-    dna_sequence = extract_sequence(file_content) #Utilisation de la fonction extract_sequence()
+    dna_sequence = extract_sequence(file_content) 
     is_dnaseq = False
     dna_sequence = ""
 
@@ -111,3 +115,43 @@ def extract_sequence(file_content):
 
     return dna_sequence
 
+# Function to construct the reverse complementary sequence
+def construct_comp_inverse(dna_sequence):
+    complement = {'a': 't', 't': 'a', 'c': 'g', 'g': 'c'}
+    complementary_sequence = ''.join([complement[base] for base in dna_sequence])
+    reverse_complement = complementary_sequence[::-1]
+    return reverse_complement
+
+def main():
+    file_name = 'NC_001133.GBK'  # Replace with your GenBank file name
+    file_content = read_file(file_name)
+    
+    if file_content:
+        print(f"Number of lines read from '{file_name}': {len(file_content)}")
+        
+        organism_name = extract_organism(file_content)
+        if organism_name:
+            print(f"Organism Name: {organism_name}")
+        else:
+            print("Organism name not found in the file.")
+        
+        genes = find_genes(file_content)
+        if genes:
+            print(f"Number of genes found: {len(genes)}")
+            sense_genes = [gene for gene in genes if gene[2] == 'sense']
+            antisense_genes = [gene for gene in genes if gene[2] == 'antisense']
+            print(f"Number of sense genes: {len(sense_genes)}")
+            print(f"Number of antisense genes: {len(antisense_genes)}")
+        else:
+            print("No genes found in the file.")
+        
+        sequence = extract_sequence(file_content)
+        if sequence:
+            print(f"Number of bases in the extracted sequence: {len(sequence)}")
+            # Constructing reverse complementary sequence for testing
+            test_sequences = ['atcg', 'AATTCCGG', 'gattaca']
+            for seq in test_sequences:
+                reverse_comp_seq = construct_comp_inverse(seq.lower())
+                print(f"Original Sequence: {seq}, Reverse Complement: {reverse_comp_seq}")
+        else:
+            print("Failed to extract sequence or sequence is empty.")
