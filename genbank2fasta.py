47b6f0e80140980df78f2e442709d2754597b8fe
Loïc P <loic.pages01@etu.umontpellier.fr>
2023-12-11 19:36:03 +0100

    Update genbank2fasta.py

--- genbank2fasta.py
+++ genbank2fasta.py
@@ -87,59 +87,6 @@ def construct_comp_inverse(dna_sequence):
     reverse_complement = complementary_sequence[::-1]
     return reverse_complement
 
-def main():
-    file_name = 'NC_001133.GBK'  # Replace with your GenBank file name
-    file_content = read_file(file_name)
-
-    if file_content:
-        # Display the number of lines read
-        print(f"Number of lines read from '{file_name}': {len(file_content)}")
-
-        # Extract the organism name
-        organism_name = extract_organism(file_content)
-        if organism_name:
-            print(f"Organism Name: {organism_name}")
-        else:
-            print("Organism name not found in the file.")
-
-        # Find and display genes
-        genes = find_genes(file_content)
-        print(f"Number of genes found: {len(genes)}")
-        
-        # Count sense and antisense genes
-        sense_genes = [gene for gene in genes if gene[2] == 'sense']
-        antisense_genes = [gene for gene in genes if gene[2] == 'antisense']
-        print(f"Number of sense genes: {len(sense_genes)}")
-        print(f"Number of antisense genes: {len(antisense_genes)}")
-        sequence = extract_sequence(file_content)
-
-        if sequence:
-            print(f"Number of bases in the extracted sequence: {len(sequence)}")
-            # Constructing reverse complementary sequence for testing
-            test_sequences = ['atcg', 'AATTCCGG', 'gattaca']
-            for seq in test_sequences:
-                reverse_comp_seq = construct_comp_inverse(seq.lower())
-                print(f"Original Sequence: {seq}, Reverse Complement: {reverse_comp_seq}")
-        else:
-            print("Failed to extract sequence or sequence is empty.")
-        #else:
-        #print("Failed to read file or file is empty.")
-
- 
-    #Use of  read_file() function
-    file_content = read_file(file_name)
-   
-# Extract gene information
-    genes = find_genes(file_content)
-    print(f"Number of genes found: {len(genes)}")
-#Use of extract_sequence() function
-    dna_sequence = extract_sequence(file_content)
-
-# use of extract_genes() function 
-    #extract_genes(genes, dna_sequence, organism_name)
-
-if __name__ == "__main__":
-    main()
     
         #WRITING A FASTA FILE
         
@@ -180,14 +127,50 @@ def extract_genes(genes, complete_sequence, organism_name):
         print(f"Gene {i} saved in '{gene_file_name}'")
 
     
-#Use of  read_file() function
-file_content = read_file(file_name)
-   
-# Extract gene information
-genes = find_genes(file_content)
+def main():
+    file_name = 'NC_001133.GBK'  # Replace with your GenBank file name
+    # Read the file
+    file_content = read_file(file_name)
 
-#Use of extract_sequence() function
-dna_sequence = extract_sequence(file_content)
+    if file_content:
+        # Display the number of lines read
+        print(f"Number of lines read from '{file_name}': {len(file_content)}")
 
-# use of extract_genes() function 
-#extract_genes(genes, dna_sequence, organism_name)
+        # Extract the organism name
+        organism_name = extract_organism(file_content)
+        if organism_name:
+            print(f"Organism Name: {organism_name}")
+        else:
+            print("Organism name not found in the file.")
+
+        # Find and display genes
+        genes = find_genes(file_content)
+        print(f"Number of genes found: {len(genes)}")
+        
+        # Count sense and antisense genes
+        sense_genes = [gene for gene in genes if gene[2] == 'sense']
+        antisense_genes = [gene for gene in genes if gene[2] == 'antisense']
+        print(f"Number of sense genes: {len(sense_genes)}")
+        print(f"Number of antisense genes: {len(antisense_genes)}")
+        sequence = extract_sequence(file_content)
+
+        extract_sequence(file_content) #affiche 0, à revoir
+
+        # Constructing reverse complementary sequence for testing
+        test_sequences = ['atcg', 'AATTCCGG', 'gattaca']
+        for seq in test_sequences:
+            reverse_comp_seq = construct_comp_inverse(seq.lower())
+            print(f"Original Sequence: {seq}, Reverse Complement: {reverse_comp_seq}")
+
+        file_n='test.fasta'
+        comment='my comment'
+        sequ='atcgatcgatcgatcgatcgatcgatcgatcgatcgatcgatcgatcgatcgatcgatcgatcgatcgatcgatcgatcgatcgatcgatcgatcg'
+        write_fasta(file_n,comment,sequence)
+
+                        #Use of extract_sequence() function
+        dna_sequence = extract_sequence(file_content)
+        extract_genes(genes,dna_sequence,organism_name)
+
+
+if __name__ == "__main__":
+    main()
