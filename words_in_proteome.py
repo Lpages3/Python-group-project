1709559a1c416af28af52f83692291a39d7ba869
Veokian <146752337+Veokian@users.noreply.github.com>
2023-12-11 22:31:13 +0000

    10001 eme commit

--- words_in_proteome.py
+++ words_in_proteome.py
@@ -1,116 +1,91 @@
-#WORDS
+# WORDS
 
-mots_filtres = []
-    
+mots_filtres = []  # creating an empty list to store filtered words
+
 def read_words(file_path):
-    
-    a = file_path
     try:
-        with open(a, 'r') as fichier:
+        with open(file_path, 'r') as fichier:
             for ligne in fichier:
-                mots = ligne.split()  # separing words by line
+                mots = ligne.split()  # splitting the words by line
                 for mot in mots:
-                    if mot.isupper() and len(mot) >= 3:
-                        mots_filtres.append(mot)
-    
-        print(mots_filtres)
-        long_mots_filtres = len(mots_filtres)
-        print("The number of selected wotds are :",long_mots_filtres)
-        return long_mots_filtres
+                    if len(mot) >= 3:
+                        mots_filtres.append(mot.upper())  # Convert words to uppercase
+
+        long_mots_filtres = len(mots_filtres)  # calculates the length of mots_filtres
+        print("The number of selected words are:", long_mots_filtres)
+        return mots_filtres
     except FileNotFoundError:
-        print(f"Fichier '{a}' introuvable.")
-        return 0
-        
-#PROTEINS
-   
-    except FileNotFoundError:
-        print("File not found.")
-def read_sequences(file_path):
+        print(f"File '{file_path}' not found.")
+        return []
+
+
+# PROTEINS
+
+def read_sequences(file_name):
     sequences = {}
-    with open(file_path, 'r') as file:
-        lines = file.readlines()
-        identifier = None
-        sequence = ''
-        for line in lines:
-            if line.startswith('>'):  # Identifying the protein identifier
-                if identifier:
-                    sequences[identifier] = sequence
-                identifier = line.strip().split('|')[0]  # Extracting protein identifier
-                sequence = ''
-            else:  # Accumulating protein sequence
-                sequence += line.strip()
-        if identifier and sequence:  # Adding the last protein sequence
-            sequences[identifier] = sequence
+    current_id = None
+    with open(file_name, 'r') as file:
+        for line in file:
+            line = line.strip()
+            if line.startswith('>'):
+                identifier = line.split('|')
+                if len(identifier) >= 2:
+                    current_id = identifier[1]
+                    sequences[current_id] = ''
+                else:
+                    pass
+            else:
+                if current_id is not None:
+                    sequences[current_id] += line
+
     return sequences
 
 
-import requests
-
-def main():
-    github_raw_url = 'https://raw.githubusercontent.com/Lpages3/Python-group-project/main/proteins.py?token=GHSAT0AAAAAACLMLSFRM44WEQFH4OH5CD4EZLUD45A'  
-    response = requests.get(github_raw_url)
-    
-    if response.status_code == 200:
-        content = response.text
-        
-        print(content)
-    else:
-        print("Erreur lors de la récupération du fichier depuis GitHub")
-
-if __name__ == "__main__":
-    main()
-
-#SEARCHING FOR WORDS 
+# SEARCHING FOR WORDS
 
 def search_words_in_proteome(mots_filtres, sequences_dict):
-    sequences_with_words = {word: 0 for word in mots_filtres} 
+    sequences_with_words = {word: {'count_sequences': 0, 'count_occurrences': 0} for word in mots_filtres}
 
-    for word in mots_filtres:
-        for sequence_id, sequence in sequences_dict.items():
-            if word in sequence:
-                sequences_with_words[word] += 1 #Insertion of 'count' method in order to compute the number of occurrences of each word in the sequences
+    for sequence_id, sequence in sequences_dict.items():
+        for word in mots_filtres:
+            occurrences = sequence.count(word)
+            if occurrences > 0:
+                sequences_with_words[word]['count_sequences'] += 1
+                sequences_with_words[word]['count_occurrences'] += occurrences
 
-    # Affichage des messages demandés
-    for word, count in sequences_with_words.items():
-        print(f"{word} found in {count['count']} sequences")
+    # Find the most frequent word
+    most_frequent_word = max(sequences_with_words, key=lambda k: sequences_with_words[k]['count_occurrences'])
+    occurrences = sequences_with_words[most_frequent_word]['count_occurrences']
+
+    print("SEARCHING FOR WORDS")
+    for word, counts in sequences_with_words.items():
+        if counts['count_sequences'] > 0:
+            print(f"{word} found in {counts['count_sequences']} sequences with {counts['count_occurrences']} occurrences.")
+
+    print(f"The most frequent word in the human proteome is '{most_frequent_word}' with {occurrences} occurrences.")
+
+    # Calculate the percentage of the proteome in which the most frequent word is found
+    nombre_total_sequences = len(sequences_dict)
+    percentage = (sequences_with_words[most_frequent_word]['count_sequences'] / nombre_total_sequences) * 100
+
+    print(f"{round(percentage, 2)}% of the proteome sequences contain the most frequent word.")
 
     return sequences_with_words
 
-#THE MOST FREQUENT WORD
 
-def find_most_frequent_word(dictionnaire_mots_sequences):
-    mot_plus_frequent = max(dictionnaire_mots_sequences, key=dictionnaire_mots_sequences.get)
-    compte = dictionnaire_mots_sequences[mot_plus_frequent]
+# Call the read_words() function
+mots_filtres = read_words('english-common-words.txt')
 
-
-    print(f"=> {mot_plus_frequent} found in {compte} séquences")
-    print("2. What is this word ?")
-    print("This word is : ",mot_plus_frequent)
-    print("What percentage of the proteome sequences contain this word ?")
-    nombre_total_sequences = len(sequences_dict)  # Assuming that sequences_dict contains all the sequences in the proteome
-    pourcentage = (compte / nombre_total_sequences) * 100
-    print(round(pourcentage, 2),"% of the proteome sequences contain this word.")
-
-#we execute every fonctions at the end of the first part : displaying all results 
-
-# Appel de la fonction read_words()
-read_words('english-common-words.txt')
-
-# Lecture des séquences de protéines
+# Read protein sequences
 sequences_dict = read_sequences('human-proteome.fasta')
 
-# display the number of sequences read
-print(f"Number of sequences read : {len(sequences_dict)}")
+# Display the number of sequences read
+print(f"Number of sequences read: {len(sequences_dict)}")
 
-# Affichage de la séquence associée à la protéine O95139 (à des fins de test)
+# Display the sequence associated with the protein O95139 (for testing purposes)
 protein_id = 'O95139'
-print("sequence associated with the protein O95139",protein_id)
-print(sequences_dict[protein_id])
+print("Sequence associated with the protein", protein_id)
+print(sequences_dict.get(protein_id, "Protein not found"))
 
-print("SEARCHING FOR WORDS")
-
-# Call the search_words_in_proteome() function and store the returned dictionary in a variable
+# Call the search_words_in_proteome() function
 sequences_with_words_dict = search_words_in_proteome(mots_filtres, sequences_dict)
-
-# Using the find_most_frequent_word() function with the returned dictionary
-find_most_frequent_word(sequences_with_words_dict)
