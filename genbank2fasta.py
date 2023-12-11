40ebbaa6d7a02b311831a365647ccc37f0b22f73
Loïc P <loic.pages01@etu.umontpellier.fr>
2023-12-11 17:50:48 +0100

    Update genbank2fasta.py

--- genbank2fasta.py
+++ genbank2fasta.py
@@ -47,7 +47,9 @@ def find_genes(file_content):
 
     return genes
 
-    
+
+
+
 #EXTRACTING THE NUCLEOTIDE SEQUENCE OF THE GENOME
 
 def extract_sequence(file_content):
@@ -80,25 +82,30 @@ def construct_comp_inverse(dna_sequence):
 def main():
     file_name = 'NC_001133.GBK'  # Replace with your GenBank file name
     file_content = read_file(file_name)
-    
+
     if file_content:
+        # Display the number of lines read
         print(f"Number of lines read from '{file_name}': {len(file_content)}")
-        
+
+        # Extract the organism name
         organism_name = extract_organism(file_content)
         if organism_name:
             print(f"Organism Name: {organism_name}")
         else:
             print("Organism name not found in the file.")
-        
+
+        # Find and display genes
         genes = find_genes(file_content)
-        if genes:
-            print(f"Number of genes found: {len(genes)}")
-            sense_genes = [gene for gene in genes if gene[2] == 'sense']
-            antisense_genes = [gene for gene in genes if gene[2] == 'antisense']
-            print(f"Number of sense genes: {len(sense_genes)}")
-            print(f"Number of antisense genes: {len(antisense_genes)}")
-        else:
-            print("No genes found in the file.")
+        print(f"Number of genes found: {len(genes)}")
+        
+        # Count sense and antisense genes
+        sense_genes = [gene for gene in genes if gene[2] == 'sense']
+        antisense_genes = [gene for gene in genes if gene[2] == 'antisense']
+        print(f"Number of sense genes: {len(sense_genes)}")
+        print(f"Number of antisense genes: {len(antisense_genes)}")
+
+    else:
+        print("Failed to read file or file is empty.")
         
         sequence = extract_sequence(file_content)
         if sequence:
@@ -111,3 +118,7 @@ def main():
         else:
             print("Failed to extract sequence or sequence is empty.")
 
+if __name__ == "__main__":
+    main()
+    
+            
